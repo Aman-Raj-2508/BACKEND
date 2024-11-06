1ff2dcbea4067417235320a450dc211fc2e485e8
Aman_Raj_2508 <144272342+Aman-Raj-2508@users.noreply.github.com>
2024-11-06 13:38:59 +0530

    Delete NODE directory

--- NODE/LECTURE-2/ES6-MODULE/tempCodeRunnerFile.js
+++
@@ -1,2 +0,0 @@
-import searching, { linearSearch, binarySearch } from './_1seachingalgo.js';
-// console.log(searching, linearSearch, binarySearch); 
--- NODE/LECTURE-2/Reading File/fsdemo.mjs
+++
@@ -1,9 +0,0 @@
-import { readFile } from 'fs/promises';
-
-const filepath = new URL('./index.html', import.meta.url, 'utf-8');
-
-// const data = await readFile(filepath);
-// console.log(data); // bufferdata
-
-const data = await readFile(filepath, { encoding: 'utf8' });
-console.log(data);//we read the whole html
