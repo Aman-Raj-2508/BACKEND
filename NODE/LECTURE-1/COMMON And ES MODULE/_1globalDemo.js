124c56bebd3e0572f012c8303492f37902611e03
AMAN SINGH <amansinghbhumihar2510@gmail.com>
2024-11-06 14:14:32 +0530

    Added backend

--- NODE/LECTURE-1/COMMON And ES MODULE/_1globalDemo.js
+++
@@ -1,6 +0,0 @@
-// console.log(document); //error
-console.log(process);
-
-console.log(__dirname);
-
-console.log(module);
--- NODE/LECTURE-2/ProcessDemo/processdemo.js
+++
@@ -1,7 +0,0 @@
-console.log("hi");
-console.log("bye");
-
-// Does not append a new line
-
-process.stdout.write("hi");
-process.stdout.write("bye");
